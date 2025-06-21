11e549229209ad330bab2845dd94f6d9a91f774e
Akas Patranobis <akaspatranobis@yahoo.com>
2025-06-21 13:03:07 +0530

    up

--- terraform/data/main.tf
+++ terraform/data/main.tf
@@ -1,7 +1,7 @@
-data "aws_security_group" "selected" {
-    name = "ct-8-allow-all"
-}
+# data "aws_security_group" "selected" {
+#     name = "ct-8-allow-all"
+# }
 
-output "sg" {
-    value = data.aws_security_group.selected
-}
+# output "sg" {
+#     value = data.aws_security_group.selected
+# }
